7b460472fad3fa435df54ab57889aa2fd0512ab7
ahmedkhan90255 <ahmed.m.khan123@gmail.com>
2024-04-18 20:25:26 -0700

    Question no.1 Completed

--- Question no.1/question1.js
+++ Question no.1/question1.js
@@ -1,6 +1,6 @@
 document.write ("<h1>Write a program that takes two user inputs for first and <br> last name using prompt and merge them in a new variable <br> titled fullName. Greet the user using his full name.</h1>")
 
-var firstName = prompt ("Enter your First Name")
-var lastName = prompt ("Enter your Last Name")
-var fullName = firstName + lastName
-console.log (fullName)
+var firstName = prompt ("Enter your First Name ")
+var lastName = prompt (" Enter your Last Name")
+
+document.write ("<h2>Ans: " + "Assalamoalaikum " + firstName + " " + lastName + "</h2>")
