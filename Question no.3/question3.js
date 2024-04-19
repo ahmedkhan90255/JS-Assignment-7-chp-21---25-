ebae85b8bd0ab02e960039091732df0ba4d24c76
ahmedkhan90255 <ahmed.m.khan123@gmail.com>
2024-04-18 20:39:13 -0700

    Question 3 completed

---
+++ Question no.3/question3.js
@@ -0,0 +1,8 @@
+document.write(
+  "<h1> Write a program to find the index of letter “n” in the word <br> “Pakistani” and display the result in your browser . </h1>"
+);
+
+var namePak = "Pakistani";
+var indexType = namePak.indexOf("n");
+
+document.write("<h2> Ans: " + indexType + "</h2>");
