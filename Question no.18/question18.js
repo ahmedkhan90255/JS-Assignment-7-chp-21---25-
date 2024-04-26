aeaccef33715cf2c2581eb74bc40dcab5134cdde
ahmedkhan90255 <ahmed.m.khan123@gmail.com>
2024-04-25 20:35:38 -0700

    Question 18

--- Question no.18/question18.js
+++ Question no.18/question18.js
@@ -1,25 +1,23 @@
-document.write("<h1> You have a string “The quick brown fox jumps over the <br> lazy dog”. Write a program to count number of <br> occurrences of word “the” in given string </h1>")
-
+document.write(
+  "<h1> You have a string “The quick brown fox jumps over the <br> lazy dog”. Write a program to count number of <br> occurrences of word “the” in given string </h1>"
+);
 
 var sentence = "The quick brown fox jumps over the lazy dog";
 var wordToCount = "the";
 
-
 var lowercaseSentence = sentence.toLowerCase();
 var lowercaseWordToCount = wordToCount.toLowerCase();
 
-
 var words = lowercaseSentence.split(" ");
-
 
 var count = 0;
 
-
 for (var i = 0; i < words.length; i++) {
-    if (words[i] === lowercaseWordToCount) {
-        count++;
-    }
+  if (words[i] === lowercaseWordToCount) {
+    count++;
+  }
 }
 
-
-document.write("<h2>Ans: Number of occurrences of '" + wordToCount + "': " + count + "</h2>");
+document.write(
+  "<h2>Ans: Number of occurrences of '" + wordToCount + "': " + count + "</h2>"
+);
